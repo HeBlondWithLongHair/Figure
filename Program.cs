984c85672f90c7042a4c385d910f440b1a2cd58e
HeBlondWithLongHair <advol231231231@gmail.com>
2023-03-13 01:29:58 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Shape[] figure = new Shape[2];
+        figure[0] = new Circle("Красный", 0, 0, 5);
+        figure[1] = new Rectangle("Черный", 0, 0, 10, 5);
+
+        foreach (Shape shape in  figure)
+        {
+            shape.Draw();
+        }
+
+        Console.ReadKey();
+    }
+}
---
+++ Shape.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+abstract public class Shape
+{
+    public string color;
+    public abstract void Draw();
+
+}
+
+public class Circle : Shape
+{
+    float x;
+    float y;
+    float radius;
+
+    public Circle(string color,float x, float y, float radius)
+    {
+        this.color = color;
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    public override void Draw()
+    {
+        Console.WriteLine($"Рисуем circle цвета: {color} с координатами ({x}, {y}) и радиусом {radius}");
+    }
+
+}
+
+public class Rectangle : Shape
+{
+   
+    float x1;
+    float x2;
+    float x3;
+    float x4;
+
+    public Rectangle(string color, float x1, float x2, float x3, float x4)
+    {
+        this.color = color;
+        this.x1 = x1;
+        this.x2 = x2;
+        this.x3 = x3;
+        this.x4 = x4;
+    }
+
+    public override void Draw()
+    {
+        Console.WriteLine($"Рисуем Rectangle цвета {color} с координатами {x1}, {x2}, {x3}, {x4}");
+    }
+
+}
